faa21fccf19a8baacb818c8013785a32090c78ef
Serena Hawkins <sjhawkins@ap.org>
2025-09-19 22:13:31 -0400

    Got mod 2 working

--- tt_gutenberg/mod2.py
+++ tt_gutenberg/mod2.py
@@ -4,4 +4,5 @@ def grouping_fun(data):
         ["language"].count().\
             to_frame().\
                 sort_values(by = "language", ascending = False)
-    return(data)
+    return_list = data.index.to_list()
+    return(return_list)
