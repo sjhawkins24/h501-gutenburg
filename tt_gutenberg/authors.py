980d3c36bc83e6e7b69bc1da288b16d077fa61f2
Serena Hawkins <sjhawkins@ap.org>
2025-09-19 22:17:51 -0400

    Trying full file

---
+++ tt_gutenberg/authors.py
@@ -0,0 +1,10 @@
+import mod2
+import mod1
+import pandas as pd
+def list_authors(by_languages=True, alias=True):
+    authors = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv")
+    lang = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv")
+    meta = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv") 
+    data = mod1.merge_data(meta, authors, lang)
+    return_list = mod2.grouping_fun(data)
+    return(return_list)   
