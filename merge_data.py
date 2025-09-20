20efd363f95f75918778a3a3577bdfa50acf0635
Serena Hawkins <sjhawkins@ap.org>
2025-09-19 21:33:04 -0400

    Starting with the first modual

---
+++ merge_data.py
@@ -0,0 +1,4 @@
+def merge_data(meta, author, lang): 
+    meta_author = pd.merge(meta, authors, on=["gutenberg_author_id", "author"])
+    data = pd.merge(meta_author, lang,  on=["gutenberg_id", "language"])
+    return(data)
