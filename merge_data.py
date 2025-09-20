af6b1413ced5f6395d9f90973a745be32864669c
Serena Hawkins <sjhawkins@ap.org>
2025-09-19 21:35:05 -0400

    Adding pandas import

--- merge_data.py
+++ merge_data.py
@@ -1,3 +1,4 @@
+import pandas as pd 
 def merge_data(meta, author, lang): 
     meta_author = pd.merge(meta, authors, on=["gutenberg_author_id", "author"])
     data = pd.merge(meta_author, lang,  on=["gutenberg_id", "language"])
