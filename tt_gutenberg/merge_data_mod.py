46ffeea597e05aae5da30e477a9e5500340c01eb
Serena Hawkins <sjhawkins@ap.org>
2025-09-19 21:49:54 -0400

    Trying a different name

--- tt_gutenberg/merge_data_mod.py
+++ tt_gutenberg/merge_data_mod.py
@@ -1,5 +1,5 @@
 import pandas as pd 
 def merge_data(meta, author, lang): 
-    meta_author = pd.merge(meta, authors, on=["gutenberg_author_id", "author"])
+    meta_author = pd.merge(meta, author, on=["gutenberg_author_id", "author"])
     data = pd.merge(meta_author, lang,  on=["gutenberg_id", "language"])
     return(data)
